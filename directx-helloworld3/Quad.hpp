3fc13739a34c82229e732d4555476da683a1f454
Lingaard <linus980331@hotmail.com>
2020-01-27 16:59:04 +0100

    remove nullptrs from quad.hpp

--- directx-helloworld3/Quad.hpp
+++ directx-helloworld3/Quad.hpp
@@ -18,14 +18,14 @@ private:
 	void createShaders(Renderer& renderer);
 	void createRenderStates(Renderer& renderer);
 
-	Microsoft::WRL::ComPtr<ID3D11Buffer> m_vertexBuffer = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> m_indexBuffer = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11Resource> m_texture = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_shaderResourceView = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11RasterizerState> m_rasterizerState = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> m_depthState = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11BlendState> m_blendState = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_indexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Resource> m_texture;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_shaderResourceView;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
+	Microsoft::WRL::ComPtr<ID3D11RasterizerState> m_rasterizerState;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> m_depthState;
+	Microsoft::WRL::ComPtr<ID3D11BlendState> m_blendState;
 };
