98bdcfab4cd8c2eece81014f62306328f34d968c
MrDoffing <nathanael.remstedt@outlook.com>
2020-01-28 10:39:22 +0100

    Added shaderSet files

---
+++ directx-helloworld3/ShaderSet.cpp
@@ -0,0 +1,172 @@
+#include "ShaderSet.h"
+
+ID3DBlob* ShaderSet::createVertexShader(LPCWSTR filename) { 
+	ID3D11Device* device = Renderer::getDevice();
+
+	ID3DBlob* pVS = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/hh968107(v=vs.85).aspx
+	HRESULT result = D3DCompileFromFile(filename, // filename
+		nullptr,								  // optional macros
+		nullptr,								  // optional include files
+		"VS_main",								  // entry point
+		"vs_5_0",								  // shader model (target)
+		D3DCOMPILE_DEBUG,						  // shader compile options (DEBUGGING)
+		0,										  // IGNORE...DEPRECATED.
+		&pVS,									  // double pointer to ID3DBlob
+		&errorBlob								  // pointer for Error Blob messages.
+	);
+
+	// compilation failed?
+	if (FAILED(result)) {
+		if (errorBlob) {
+			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+			// release "reference" to errorBlob interface object
+			errorBlob->Release();
+		}
+		if (pVS)
+			pVS->Release();
+		// return result;
+	}
+
+	result = device->CreateVertexShader(pVS->GetBufferPointer(), pVS->GetBufferSize(), nullptr, &m_vertexShader);
+	return pVS;
+}
+
+HRESULT ShaderSet::createGeometryShader(LPCWSTR filename) {
+	ID3D11Device* device = Renderer::getDevice();
+
+	ID3DBlob* pGS = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	HRESULT result =
+		D3DCompileFromFile(filename, nullptr, nullptr, "GS_main", "gs_5_0", D3DCOMPILE_DEBUG, 0, &pGS, &errorBlob);
+
+	// compilation failed?
+	if (FAILED(result)) {
+		if (errorBlob) {
+			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+			// release "reference" to errorBlob interface object
+			errorBlob->Release();
+		}
+		if (pGS)
+			pGS->Release();
+		return result;
+	}
+
+	device->CreateGeometryShader(pGS->GetBufferPointer(), pGS->GetBufferSize(), nullptr, &m_geometryShader);
+
+	pGS->Release();
+}
+
+HRESULT ShaderSet::createFragmentShader(LPCWSTR filename) {
+	ID3D11Device* device = Renderer::getDevice();
+
+	ID3DBlob* pPS = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	HRESULT result = D3DCompileFromFile(filename, // filename
+		nullptr,								  // optional macros
+		nullptr,								  // optional include files
+		"PS_main",								  // entry point
+		"ps_5_0",								  // shader model (target)
+		D3DCOMPILE_DEBUG,						  // shader compile options
+		0,										  // effect compile options
+		&pPS,									  // double pointer to ID3DBlob
+		&errorBlob								  // pointer for Error Blob messages.
+	);
+
+	// compilation failed?
+	if (FAILED(result)) {
+		if (errorBlob) {
+			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+			// release "reference" to errorBlob interface object
+			errorBlob->Release();
+		}
+		if (pPS)
+			pPS->Release();
+		return result;
+	}
+
+	device->CreatePixelShader(pPS->GetBufferPointer(), pPS->GetBufferSize(), nullptr, &m_pixelShader);
+	// we do not need anymore this COM object, so we release it.
+	pPS->Release();
+}
+
+bool ShaderSet::isLoaded() const { 
+	return m_loaded; 
+}
+
+bool ShaderSet::createShaders(LPCWSTR vertexName, LPCWSTR geometryName, LPCWSTR fragmentName,
+	D3D11_INPUT_ELEMENT_DESC* inputDesc, int inputDescCount) {
+	
+	ID3D11Device* device = Renderer::getDevice();
+
+	m_loaded = true;
+	bool check = true;
+	ID3DBlob* pVS = createVertexShader(vertexName);
+	HRESULT res;
+	if (inputDesc == nullptr) {
+		D3D11_INPUT_ELEMENT_DESC standardInputDesc[] = {
+			{
+				"Position",					 // "semantic" name in shader
+				0,							 // "semantic" index (not used)
+				DXGI_FORMAT_R32G32B32_FLOAT, // size of ONE element (3 floats)
+				0,							 // input slot
+				0,							 // offset of first element
+				D3D11_INPUT_PER_VERTEX_DATA, // specify data PER vertex
+				0							 // used for INSTANCING (ignore)
+			},
+			{ "TexCoordinate", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "Normal", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 20, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+		};
+		res = device->CreateInputLayout(standardInputDesc, ARRAYSIZE(standardInputDesc), pVS->GetBufferPointer(),
+			pVS->GetBufferSize(), &m_vertexLayout);
+		if (FAILED(res))
+			check = false;
+	}
+	else {
+		res = device->CreateInputLayout(
+			inputDesc, inputDescCount, pVS->GetBufferPointer(), pVS->GetBufferSize(), &m_vertexLayout);
+		if (FAILED(res))
+			check = false;
+	}
+	pVS->Release();
+	if (geometryName != nullptr && FAILED(createGeometryShader(geometryName)))
+		check = false;
+	if (fragmentName != nullptr && FAILED(createFragmentShader(fragmentName)))
+		check = false;
+	return check;
+}
+
+void ShaderSet::bindShadersAndLayout() {
+	ID3D11DeviceContext* deviceContext = Renderer::getDeviceContext();
+
+	deviceContext->VSSetShader(m_vertexShader, nullptr, 0);
+	deviceContext->HSSetShader(nullptr, nullptr, 0);
+	deviceContext->DSSetShader(nullptr, nullptr, 0);
+	deviceContext->GSSetShader(m_geometryShader, nullptr, 0);
+	deviceContext->PSSetShader(m_pixelShader, nullptr, 0);
+
+	deviceContext->IASetInputLayout(m_vertexLayout);
+}
+
+void ShaderSet::release() {
+	if (m_vertexShader != nullptr)
+		m_vertexShader->Release();
+	if (m_geometryShader != nullptr)
+		m_geometryShader->Release();
+	if (m_pixelShader != nullptr)
+		m_pixelShader->Release();
+	if (m_vertexLayout != nullptr)
+		m_vertexLayout->Release();
+}
+
+ShaderSet::ShaderSet(LPCWSTR vertexName, LPCWSTR geometryName, LPCWSTR fragmentName, D3D11_INPUT_ELEMENT_DESC* inputDesc, int inputDescCount) {
+	createShaders(vertexName, geometryName, fragmentName, inputDesc, inputDescCount);
+}
+
+ShaderSet::ShaderSet() {}
+
+ShaderSet::~ShaderSet() { release(); }
---
+++ directx-helloworld3/ShaderSet.h
@@ -0,0 +1,24 @@
+#include "Renderer.hpp"
+#include <d3dcompiler.h>
+
+class ShaderSet {
+protected:
+	bool m_loaded = false;
+	ID3D11InputLayout* m_vertexLayout = nullptr;
+
+	ID3D11VertexShader* m_vertexShader = nullptr;
+	ID3D11GeometryShader* m_geometryShader = nullptr;
+	ID3D11PixelShader* m_pixelShader = nullptr;
+	ID3DBlob* createVertexShader(LPCWSTR filename);
+	HRESULT createGeometryShader(LPCWSTR filename);
+	HRESULT createFragmentShader(LPCWSTR filename);
+
+public:
+	bool isLoaded() const;
+	bool createShaders(LPCWSTR vertexName, LPCWSTR geometryName, LPCWSTR fragmentName, D3D11_INPUT_ELEMENT_DESC* inputDesc = nullptr, int inputDescCount = 0);
+	void bindShadersAndLayout();
+	void release();
+	ShaderSet(LPCWSTR vertexName, LPCWSTR geometryName, LPCWSTR fragmentName, D3D11_INPUT_ELEMENT_DESC* inputDesc = nullptr, int inputDescCount = 0);
+	ShaderSet();
+	~ShaderSet();
+};
