3933a8bcbbecd9879fc3e3ffb819d311bd4128bb
frozenbanana <henrybergstrom@protonmail.com>
2020-01-29 10:49:43 +0100

    removed dubugging comments

--- fruithunter/IntroState.cpp
+++ fruithunter/IntroState.cpp
@@ -11,7 +11,6 @@ void IntroState::handleEvent(int event) { return; }
 
 void IntroState::pause() { ErrorLogger::log(m_name + " pause() called."); }
 
-void IntroState::draw() { /*ErrorLogger::log(m_name + " draw() called.");*/
-}
+void IntroState::draw() { ErrorLogger::log(m_name + " draw() called."); }
 
 void IntroState::play() { ErrorLogger::log(m_name + " play() called."); }
