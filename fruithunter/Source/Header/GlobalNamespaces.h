efa1a3fe8a572aebc12985abe6e883b5634c2458
MrDoffing <nathanael.remstedt@outlook.com>
2020-03-01 20:58:23 +0100

    moved back the defines of the far and near plane to globalNamespaces.h

--- fruithunter/Source/Header/GlobalNamespaces.h
+++ fruithunter/Source/Header/GlobalNamespaces.h
@@ -22,6 +22,9 @@ using float4x4 = DirectX::SimpleMath::Matrix;
 #define STANDARD_CORNER_X 0
 #define STANDARD_CORNER_Y 0
 
+#define NEAR_PLANE 0.1f
+#define FAR_PLANE 100.f
+
 enum FruitType { APPLE, BANANA, MELON, NR_OF_FRUITS };
 enum TimeTargets { GOLD, SILVER, BRONZE, NR_OF_TIME_TARGETS };
 
--- fruithunter/Source/Utility/Camera/Camera.h
+++ fruithunter/Source/Utility/Camera/Camera.h
@@ -3,9 +3,6 @@
 
 #define MATRIX_SLOT 1
 
-#define NEAR_PLANE 0.1f
-#define FAR_PLANE 100.f
-
 class Camera {
 public:
 	Camera();
