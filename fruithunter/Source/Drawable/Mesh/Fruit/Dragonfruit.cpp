c2b7fd76a711eae12721ea8d466a016a56a44cb6
Jan-Olof Blime Strömberg <JO.B.Stromberg@hotmail.com>
2020-03-11 11:52:32 +0100

    slow merge with dragonfruitmodel

--- fruithunter/Source/Drawable/Mesh/Fruit/Dragonfruit.cpp
+++ fruithunter/Source/Drawable/Mesh/Fruit/Dragonfruit.cpp
@@ -1,8 +1,8 @@
-#include "Dragonfruit.h"
+#include "DragonFruit.h"
 
-Dragonfruit::Dragonfruit(float3 pos) {}
+DragonFruit::DragonFruit(float3 pos) {}
 
-void Dragonfruit::waveFlight(float3 playerPosition, float radius) {
+void DragonFruit::waveFlight(float3 playerPosition, float radius) {
 
 
 	int degrees = 10 % 360;
@@ -10,7 +10,7 @@ void Dragonfruit::waveFlight(float3 playerPosition, float radius) {
 	float flightPattern = sin(radians);
 }
 
-void Dragonfruit::circulateVertical(float3 playerPosition, float radius) {
+void DragonFruit::circulateVertical(float3 playerPosition, float radius) {
 	float3 toMelon = m_position - playerPosition;
 	toMelon.y = playerPosition.y;
 	float angle = XM_PI / 8;
@@ -26,9 +26,9 @@ void Dragonfruit::circulateVertical(float3 playerPosition, float radius) {
 	target.y = playerPosition.y;
 }
 
-void Dragonfruit::circulate(float3 playerDir, float radius) {}
+void DragonFruit::circulate(float3 playerDir, float radius) {}
 
-void Dragonfruit::behaviorPassive(float3 playerPosition) {
+void DragonFruit::behaviorPassive(float3 playerPosition) {
 	// Perch on ground or on tree?
 	if (withinDistanceTo(playerPosition, m_passiveRadius)) {
 		changeState(ACTIVE);
@@ -50,12 +50,54 @@ void Dragonfruit::behaviorPassive(float3 playerPosition) {
 	m_speed = m_passive_speed;
 }
 
-void Dragonfruit::behaviorActive(float3 playerPosition) {
+void DragonFruit::behaviorActive(float3 playerPosition) {
 	// when player is near, take flight
 
 	// circulate player in air.
 }
 
-void Dragonfruit::behaviorCaught(float3 playerPosition) {
+void DragonFruit::behaviorCaught(float3 playerPosition) {
 	// just go to player
+}
+
+
+DragonFruit::DragonFruit() {
+	loadAnimated("Dragon", 3);
+	vector<string> names{ "Dragon1.mtl", "Dragon2.mtl", "Dragon3.mtl" };
+	loadMaterials(names, 3);
+
+	m_nrOfFramePhases = 2;
+
+	changeState(AI::State::PASSIVE);
+
+	m_fruitType = DRAGON;
+
+	// TEMP TAKEN FROM APPLE
+	m_activeRadius = 8.f;
+	m_passiveRadius = 12.f;
+
+	m_passive_speed = 3.f;
+	m_active_speed = 10.f;
+	m_caught_speed = 5.f;
+
+	setCollisionDataOBB();
+}
+
+DragonFruit::~DragonFruit() {}
+
+void DragonFruit::updateAnimated(float dt) {
+	m_frameTime += dt * 4;
+	m_frameTime = fmodf(m_frameTime, 4.f);
+
+	if (m_frameTime < 1)
+		setFrameTargets(0, 1);
+	else if (m_frameTime < 2)
+		setFrameTargets(1, 2);
+	else if (m_frameTime < 3)
+		setFrameTargets(2, 1);
+	else if (m_frameTime < 4)
+		setFrameTargets(1, 0);
+	m_meshAnim.updateSpecific(m_frameTime);
+
+	return;
 }
--- fruithunter/Source/Drawable/Mesh/Fruit/Dragonfruit.h
+++ fruithunter/Source/Drawable/Mesh/Fruit/Dragonfruit.h
@@ -3,7 +3,7 @@
 #include "Fruit.h"
 
 #define MAXNROFJUMPS 2
-class Dragonfruit : public Fruit {
+class DragonFruit : public Fruit {
 private:
 	void behaviorPassive(float3 playerPosition) override;
 	void behaviorActive(float3 playerPosition) override;
@@ -19,7 +19,7 @@ private:
 	bool inFlight;
 
 public:
-	Dragonfruit(float3 pos = float3(12.f, 0.f, 6.f));
+	DragonFruit(float3 pos = float3(12.f, 0.f, 6.f));
 	void updateAnimated(float dt);
 	void waveFlight(float3 playerDir, float radius);
 	void circulateVertical(float3 playerDir, float radius);
