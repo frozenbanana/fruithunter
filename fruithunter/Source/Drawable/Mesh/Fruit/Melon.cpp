97089ff3ba72d79046cb174be945e72d0571915a
Filip Unger <filip.unger@hotmail.se>
2020-03-16 15:29:21 +0100

    quickfix for crashing at goldshots

--- fruithunter/Source/Drawable/Mesh/Fruit/Melon.cpp
+++ fruithunter/Source/Drawable/Mesh/Fruit/Melon.cpp
@@ -4,8 +4,8 @@ Melon::Melon(float3 pos) : Fruit(pos) {
 	loadAnimated("Melon", 1);
 	m_nrOfFramePhases = 6;
 	vector<string> names{ "Melon.mtl", "Melon2bronze.mtl", "Melon2silver.mtl", "Melon2gold.mtl",
-		"Melon.mtl" };
-	loadMaterials(names, 3);
+		"Melon3.mtl" };
+	loadMaterials(names, 5);
 
 	m_meshAnim.setFrameTargets(0, 0);
 	m_rollSpeed = 5.f;
