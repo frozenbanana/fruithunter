44e234ce24ccfcf9b9481ddf08783dcde8b2ac23
MrDoffing <nathanael.remstedt@outlook.com>
2020-10-08 00:58:29 +0200

    weird commit

--- fruithunter/Source/Drawable/Mesh/Fruit/DragonFruit.h
+++ fruithunter/Source/Drawable/Mesh/Fruit/DragonFruit.h
@@ -6,9 +6,9 @@
 
 class DragonFruit : public Fruit {
 private:
-	void behaviorPassive(float3 playerPosition) override;
-	void behaviorActive(float3 playerPosition) override;
-	void behaviorCaught(float3 playerPosition) override;
+	void behaviorPassive() override;
+	void behaviorActive() override;
+	void behaviorCaught() override;
 	float3 m_target;
 	float m_offsetFromGround;
 	float m_wingStrength;
@@ -17,6 +17,18 @@ private:
 	bool isFalling();
 	bool m_ascend = false;
 	bool updated = false;
+
+	float2 m_spawn_range = float2(5,35); // additional height above terrain height
+
+	float m_heightFromGroundLimit = 20; // If under limit, will force fruit to fly away from ground
+	float m_velocitySpeed = 10; // flight speed
+	float m_speedGain = 8; // speed acquired from tilting
+	float m_steeringSpeed = 1.2; // speed of turning to target
+	float m_turningStrength = 50; // z rotation strength from turning
+	float m_rotZ = 0; // current z rotation
+
+	float m_timer = 0; // time until new target 
+	float2 m_timer_range = float2(3,5); // timer length range
 
 public:
 	DragonFruit(float3 pos = float3(12.f, 0.f, 6.f));
