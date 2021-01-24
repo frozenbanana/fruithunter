2290df57fc3271595c6b3e7a6e46563ab74a5b42
MrDoffing <nathanael.remstedt@outlook.com>
2021-01-25 00:51:11 +0100

    Fixed HUD, swiched out old code to use new class abstractions. It also now fits with the screen.

--- fruithunter/Source/Drawable/HUD.cpp
+++ fruithunter/Source/Drawable/HUD.cpp
@@ -31,136 +31,70 @@ void HUD::drawTargetTime() {
 	string timeString = "Target: " + Time2DisplayableString(target) + ".00";
 	//draw target time
 	wstring w_timeString = std::wstring(timeString.begin(), timeString.end());
-	m_spriteFont->DrawString(m_spriteBatch.get(), w_timeString.c_str(), float2(30.0f, SCREEN_HEIGHT - 130.0f), color);
-}
-
-void HUD::setDepthStateToNull() {
-	Renderer::getInstance()->getDeviceContext()->OMSetDepthStencilState(nullptr, 0);
+	m_textRenderer.setColor(color);
+	m_textRenderer.setScale(0.45);
+	m_textRenderer.setAlignment(TextRenderer::LEFT, TextRenderer::CENTER);
+	m_textRenderer.draw(timeString, float2(30, 720-100));
 }
 
 HUD::HUD() {
-	// Text font
-	m_spriteFont = std::make_unique<DirectX::SpriteFont>(
-		Renderer::getDevice(), L"assets/fonts/myfile2.spritefont");
+	m_img_background.load("background.png");
+	m_img_background.setPosition(float2(20, 720-130));
+	m_img_stamina.load("stamina.png");
+	m_img_stamina.setPosition(float2(1280-230, 720-60));
+	m_img_staminaFrame.load("staminaFrame.png");
+	m_img_staminaFrame.setPosition(m_img_stamina.getPosition() + float2(-13, -10));
+	m_img_staminaFrame.setScale(float2(1.05, 0.8));
 
-	if (!m_spriteFont.get()) {
-		ErrorLogger::log("HUD failed to load font.");
-		return;
+	Color textColors[NR_OF_FRUITS] = { Color(1, 0, 0, 1), Color(0.9, 0.7, 0.2, 1),
+		Color(0.4, 0.7, 0.3, 1), Color(1, 0.3, 0.3, 1) };
+	string fruitImages[NR_OF_FRUITS] = { "apple.png", "banana.png", "melon.png",
+		"dragonfruit.png" };
+	for (size_t i = 0; i < NR_OF_FRUITS; i++) {
+		m_fruitSprites[i].sprite.load(fruitImages[i]);
+		m_fruitSprites[i].sprite.setAlignment(); // center
+		m_fruitSprites[i].sprite.setSize(float2(1.)*50);
+		m_fruitSprites[i].textColor = textColors[i];
 	}
-
-	// Text colors for fruit inventory
-	m_fruitTextColors[APPLE] = { 1.f, 0.f, 0.f, 1.f };
-	m_fruitTextColors[BANANA] = { 0.9f, 0.7f, 0.2f, 1.f };
-	m_fruitTextColors[MELON] = { 0.4f, 0.7f, 0.3f, 1.f };
-	m_fruitTextColors[DRAGON] = { 1.0f, 0.3f, 0.3f, 1.f };
-
-	m_spriteBatch = std::make_unique<SpriteBatch>(Renderer::getDeviceContext());
-	m_states = std::make_unique<CommonStates>(Renderer::getDevice());
-
-	Microsoft::WRL::ComPtr<ID3D11Resource> resource;
-
-	HRESULT t = CreateWICTextureFromFile(Renderer::getDevice(), L"assets/sprites/background.png",
-		resource.GetAddressOf(), m_backgroundTexture.ReleaseAndGetAddressOf());
-	if (FAILED(t))
-		ErrorLogger::logError("Failed to create backgorund sprite texture", t);
-
-
-	t = CreateWICTextureFromFile(Renderer::getDevice(), L"assets/sprites/stamina.png",
-		resource.GetAddressOf(), m_staminaTexture.ReleaseAndGetAddressOf());
-	if (FAILED(t))
-		ErrorLogger::logError("Failed to create stamina sprite texture", t);
-
-
-	t = CreateWICTextureFromFile(Renderer::getDevice(), L"assets/sprites/staminaFrame.png",
-		resource.GetAddressOf(), m_staminaFrame.ReleaseAndGetAddressOf());
-	if (FAILED(t))
-		ErrorLogger::logError("Failed to create stamina frame texture", t);
-
-	// create on screen fruit items
-	initilizeFruitItem(m_fruitSprites[APPLE], "apple.png", float4(1.f, 0.f, 0.f, 1.f));
-	initilizeFruitItem(m_fruitSprites[BANANA], "banana.png", float4(0.9f, 0.7f, 0.2f, 1.f));
-	initilizeFruitItem(m_fruitSprites[MELON], "melon.png", float4(0.4f, 0.7f, 0.3f, 1.f));
-	initilizeFruitItem(m_fruitSprites[DRAGON], "dragonfruit.png", float4(1.0f, 0.3f, 0.3f, 1.f));
-}
-
-HUD::~HUD() {
-	m_spriteBatch.reset();
-	m_states.reset();
-}
-
-void HUD::initilizeFruitItem(OnScreenFruitItem& item, string imageFilename, float4 textColor) {
-	
-	Microsoft::WRL::ComPtr<ID3D11Resource> resource;
-
-	wstring fileName = wstring(imageFilename.begin(), imageFilename.end());
-	wstring filePath = L"assets/sprites/" + fileName;
-
-	HRESULT t = CreateWICTextureFromFile(Renderer::getDevice(), filePath.c_str(),
-		resource.GetAddressOf(), item.texture.ReleaseAndGetAddressOf());
-	if (FAILED(t))
-		ErrorLogger::logError("Failed to create fruit sprite texture", t);
-
-	Microsoft::WRL::ComPtr<ID3D11Texture2D> tex;
-	resource.As(&tex);
-	CD3D11_TEXTURE2D_DESC texDesc;
-	tex->GetDesc(&texDesc);
-
-	// Set all sprites to the same size
-	item.scale = 75.0f / (float)texDesc.Height;
-	// color
-	item.textColor = textColor;
 }
 
 void HUD::draw() {
-	m_spriteBatch->Begin(SpriteSortMode_Deferred, m_states->NonPremultiplied());
-	m_backgroundPos = float2(20.0f, SCREEN_HEIGHT - 130.0f);
-	m_staminaPos = float2(SCREEN_WIDTH - 230.0f, SCREEN_HEIGHT - 60.0f);
+	float stamina = SceneManager::getScene()->m_player->getStamina();
 
 	// Draw text background
-	float stamina = SceneManager::getScene()->m_player->getStamina();
-	m_spriteBatch->Draw(m_backgroundTexture.Get(), m_backgroundPos);
-	m_spriteBatch->Draw(m_staminaTexture.Get(), m_staminaPos, nullptr, Colors::White, 0.0f,
-		float2(0.0f, 0.0f), float2(stamina + 0.05f, 0.8f));
-	m_spriteBatch->Draw(m_staminaFrame.Get(),
-		float2(m_staminaPos.x - 13.0f, m_staminaPos.y - 10.0f), nullptr, Colors::White, 0.0f,
-		float2(0.0f, 0.0f), float2(1.05f, 0.8f));
-
-	m_spriteBatch->End();
-
-	m_spriteBatch->Begin();
+	m_img_background.draw();
+	m_img_stamina.setScale(float2(stamina + 0.05, 0.8));
+	m_img_stamina.draw();
+	m_img_staminaFrame.draw();
 
 	// Draw time and target time
 	float time = SceneManager::getScene()->m_timer.getTimePassed();
 	size_t rest = int((time - int(time)) * 100);
-	string timeString = "   Time: " + Time2DisplayableString((size_t)time)+"."+(rest<10?"0":"")+to_string(rest);
-	wstring w_timeString = wstring(timeString.begin(), timeString.end());
-	m_spriteFont->DrawString(
-		m_spriteBatch.get(), w_timeString.c_str(), float2(30.0f, SCREEN_HEIGHT - 80.0f));
+	string timeString = "    Time: " + Time2DisplayableString((size_t)time)+"."+(rest<10?"0":"")+to_string(rest);
+	m_textRenderer.setColor(float4(1.));
+	m_textRenderer.setScale(0.45);
+	m_textRenderer.setAlignment(TextRenderer::LEFT, TextRenderer::CENTER);
+	m_textRenderer.draw(timeString, float2(30, 720 - 60));
 	drawTargetTime();
 
 	// Draw inventory numbers and fruit sprites
 	int* gathered = SceneManager::getScene()->m_gatheredFruits;		  // NR_OF_FRUITS
 	int* winCondition = SceneManager::getScene()->m_utility.winCondition; // NR_OF_FRUITS
-	float2 itemPosition = float2(25, 25);
-	float2 itemOffset = float2(0, 100);
+	float2 itemPosition = float2(50, 50);
+	float2 itemOffset = float2(0, 75);
 	for (size_t i = 0; i < NR_OF_FRUITS; i++) {
 		if (winCondition[i] > 0) {
 			//text
-			wstring displayText = to_wstring(gathered[i]) + L"/" + to_wstring(winCondition[i]);
-			m_spriteFont->DrawString(m_spriteBatch.get(), displayText.c_str(),
-				itemPosition + float2(75.0f, 0.0f), m_fruitSprites[i].textColor);
+			string displayStr = to_string(gathered[i]) + "/" + to_string(winCondition[i]);
+			m_textRenderer.setColor(m_fruitSprites[i].textColor);
+			m_textRenderer.setScale(0.6);
+			m_textRenderer.setAlignment(TextRenderer::LEFT, TextRenderer::CENTER);
+			m_textRenderer.draw(displayStr, itemPosition + float2(35, 0));
 			//image
-			m_spriteBatch->Draw(m_fruitSprites[i].texture.Get(), itemPosition, nullptr,
-				Colors::White, 0.f, float2(0.0f, 0.0f),
-				m_fruitSprites[i].scale + 0.1f * m_fruitSprites[i].extraScaling);
+			m_fruitSprites[i].sprite.setPosition(itemPosition);
+			m_fruitSprites[i].sprite.draw();
 			//next item preparation
 			itemPosition += itemOffset;
 		}
 	}
-
-
-	m_spriteBatch->End();
-
-	// Reset depth state
-	setDepthStateToNull();
 }
--- fruithunter/Source/Drawable/HUD.h
+++ fruithunter/Source/Drawable/HUD.h
@@ -1,27 +1,12 @@
 #pragma once
 #include "GlobalNamespaces.h"
-#include <SpriteBatch.h>
-#include <CommonStates.h>
-#include <SpriteFont.h>
+#include "Sprite2D.h"
+#include "TextRenderer.h"
 
 class HUD {
 private:
-	struct Sprite {
-		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;
-		float2 screenPos = float2(0, 0);
-		float scale = 1;
-		FruitType fruitType = FruitType::APPLE;
-		float pickUp = 0; // scales fruit sprite when fruit is picked up. 
-	};
-
-	unique_ptr<SpriteBatch> m_spriteBatch;
-	unique_ptr<SpriteFont> m_spriteFont;
-	unique_ptr<CommonStates> m_states;
-
-	//vector<Sprite> m_sprites;
-
 	struct OnScreenFruitItem {
-		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;
+		Sprite2D sprite;
 		float scale = 1;
 		float extraScaling = 0; // scales fruit sprite when fruit is picked up. 
 		float4 textColor = float4(1.);
@@ -32,22 +17,16 @@ private:
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_staminaTexture;
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_staminaFrame;
 
-	float2 m_backgroundPos;
-	float2 m_staminaPos;
-	float2 m_timerPos;
-	XMVECTORF32 m_fruitTextColors[NR_OF_FRUITS];
+	Sprite2D m_img_background, m_img_stamina, m_img_staminaFrame;
+
+	TextRenderer m_textRenderer;
 
 	//-- Private Functions --
 
-	void initilizeFruitItem(OnScreenFruitItem& item, string imageFilename, float4 textColor);
-
 	void drawTargetTime();
-
-	void setDepthStateToNull();
 
 public:
 	HUD();
-	~HUD();
 
 	void draw();
 };
