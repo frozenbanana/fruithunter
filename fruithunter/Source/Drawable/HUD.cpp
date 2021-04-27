735437c92e2bf3580810a714c4329e2d88f3d083
Nathanael Remstedt <nathanael.remstedt@outlook.com>
2021-04-27 17:38:32 +0200

    replaced fruit catch count hud elements

--- fruithunter/Source/Drawable/HUD.cpp
+++ fruithunter/Source/Drawable/HUD.cpp
@@ -39,6 +39,17 @@ void HUD::drawTargetTime() {
 	m_text.draw();
 }
 
+void HUD::imgui_tickSetting() {
+	if (ImGui::Begin("TickSettings")) {
+		ImGui::SliderFloat("slowdown", &m_tickSetting.slowdown, 0, 1);
+		ImGui::InputFloat("gravity", &m_tickSetting.gravity);
+		ImGui::InputFloat("kickoff", &m_tickSetting.kickoff);
+		ImGui::InputFloat("offset w", &m_tickSetting.offset_width);
+		ImGui::InputFloat("offset h", &m_tickSetting.offset_height);
+		ImGui::End();
+	}
+}
+
 HUD::HUD() {
 	m_img_background.load("background.png");
 	m_img_background.setPosition(float2(20, 720-130));
@@ -47,6 +58,21 @@ HUD::HUD() {
 	m_img_staminaFrame.load("staminaFrame.png");
 	m_img_staminaFrame.setPosition(m_img_stamina.getPosition() + float2(-13, -10));
 	m_img_staminaFrame.setScale(float2(1.05f, 0.8f));
+
+	m_hd_back.load("hd_back.png");
+	m_hd_back.setPosition(float2(0, 0));
+	m_hd_back.setScale(0.35f);
+	m_hd_ticks[FruitType::APPLE].load("appleTick.png");
+	m_hd_ticks[FruitType::BANANA].load("bananaTick.png");
+	m_hd_ticks[FruitType::MELON].load("melonTick.png");
+	m_hd_ticks[FruitType::DRAGON].load("dragonTick.png");
+	for (size_t i = 0; i < FruitType::NR_OF_FRUITS; i++) {
+		m_hd_ticks[i].setAlignment(); // center
+		m_hd_ticks[i].setScale(m_tickSetting.base_scale);
+	}
+	m_hd_emptyTick.load("emptyTick.png");
+	m_hd_emptyTick.setAlignment(); // center
+	m_hd_emptyTick.setScale(m_tickSetting.base_scale);
 
 	Color textColors[NR_OF_FRUITS] = { Color(1.0f, 0.0f, 0.0f, 1.0f), Color(0.9f, 0.7f, 0.2f, 1.0f),
 		Color(0.4f, 0.7f, 0.3f, 1.0f), Color(1.0f, 0.3f, 0.3f, 1.0f) };
@@ -58,6 +84,37 @@ HUD::HUD() {
 		m_fruitSprites[i].sprite.setSize(float2(1.)*50);
 		m_fruitSprites[i].textColor = textColors[i];
 	}
+}
+
+void HUD::update(float dt) {
+
+	for (size_t f = 0; f < FruitType::NR_OF_FRUITS; f++) {
+		int count_target = SceneManager::getScene()->m_utility.winCondition[f];
+		int count_current = SceneManager::getScene()->m_gatheredFruits[f];
+		int count_previous = m_tick_previousCatchCount[f];
+		// resize if needed
+		if (m_tickAnimations[f].size() < count_target) {
+			m_tickAnimations[f].resize(count_target); // resize to fit
+			m_tick_previousCatchCount[f] = 0;
+		}
+		// ticks
+		for (size_t i = 0; i < m_tickAnimations[f].size(); i++) {
+			// activator
+			if (i >= count_previous && i < count_current) {
+				m_tickAnimations[f][i].scalingVelocity = m_tickSetting.kickoff;
+			}
+			// animation
+			float desiredScale = (i < count_current) ? 1 : 0.75f;
+			m_tickAnimations[f][i].scalingVelocity +=
+				(desiredScale - m_tickAnimations[f][i].scale) * m_tickSetting.gravity *
+				dt;																		 // gravity
+			m_tickAnimations[f][i].scale += m_tickAnimations[f][i].scalingVelocity * dt; // movement
+			m_tickAnimations[f][i].scalingVelocity *= pow(m_tickSetting.slowdown, dt);	 // slowdown
+		}
+		// update catch count
+		m_tick_previousCatchCount[f] = count_current;
+	}
+
 }
 
 void HUD::draw() {
@@ -82,25 +139,44 @@ void HUD::draw() {
 	drawTargetTime();
 
 	// Draw inventory numbers and fruit sprites
-	int* gathered = SceneManager::getScene()->m_gatheredFruits;		  // NR_OF_FRUITS
-	int* winCondition = SceneManager::getScene()->m_utility.winCondition; // NR_OF_FRUITS
-	float2 itemPosition = float2(50, 50);
-	float2 itemOffset = float2(0, 75);
-	for (size_t i = 0; i < NR_OF_FRUITS; i++) {
-		if (winCondition[i] > 0) {
-			//text
-			string displayStr = to_string(gathered[i]) + "/" + to_string(winCondition[i]);
-			m_text.setColor(m_fruitSprites[i].textColor);
-			m_text.setScale(0.6f);
-			m_text.setAlignment(HorizontalAlignment::AlignLeft, VerticalAlignment::AlignCenter);
-			m_text.setPosition(itemPosition + float2(35.0f, 0.0f));
-			m_text.setText(displayStr);
-			m_text.draw();
-			//image
-			m_fruitSprites[i].sprite.setPosition(itemPosition);
-			m_fruitSprites[i].sprite.draw();
-			//next item preparation
-			itemPosition += itemOffset;
+	//int* gathered = SceneManager::getScene()->m_gatheredFruits;		  // NR_OF_FRUITS
+	//int* winCondition = SceneManager::getScene()->m_utility.winCondition; // NR_OF_FRUITS
+	//float2 itemPosition = float2(50, 50);
+	//float2 itemOffset = float2(0, 75);
+	//for (size_t i = 0; i < NR_OF_FRUITS; i++) {
+	//	if (winCondition[i] > 0) {
+	//		//text
+	//		string displayStr = to_string(gathered[i]) + "/" + to_string(winCondition[i]);
+	//		m_text.setColor(m_fruitSprites[i].textColor);
+	//		m_text.setScale(0.6f);
+	//		m_text.setAlignment(HorizontalAlignment::AlignLeft, VerticalAlignment::AlignCenter);
+	//		m_text.setPosition(itemPosition + float2(35.0f, 0.0f));
+	//		m_text.setText(displayStr);
+	//		m_text.draw();
+	//		//image
+	//		m_fruitSprites[i].sprite.setPosition(itemPosition);
+	//		m_fruitSprites[i].sprite.draw();
+	//		//next item preparation
+	//		itemPosition += itemOffset;
+	//	}
+	//}
+
+
+	m_hd_back.draw();
+	float2 backSize = m_hd_back.getSize();
+	float2 tickSize = m_hd_emptyTick.getSize();
+	for (size_t f = 0; f < FruitType::NR_OF_FRUITS; f++) {
+		float mid = m_hd_back.getSize().y / 2;
+		float2 baseTickPos = float2(backSize.x - backSize.x / 12,
+			mid + m_tickSetting.offset_height * f -
+				(m_tickSetting.offset_height * (FruitType::NR_OF_FRUITS - 1)) / 2);
+		int count_target = SceneManager::getScene()->m_utility.winCondition[f];
+		int count_current = SceneManager::getScene()->m_gatheredFruits[f];
+		for (size_t t = 0; t < count_target; t++) {
+			Sprite2D* spr = (t < count_current) ? &m_hd_ticks[f] : &m_hd_emptyTick;
+			spr->setPosition(baseTickPos + float2(t * m_tickSetting.offset_width, 0));
+			spr->setScale(m_tickSetting.base_scale * m_tickAnimations[f][t].scale);
+			spr->draw();
 		}
 	}
 }
--- fruithunter/Source/Drawable/HUD.h
+++ fruithunter/Source/Drawable/HUD.h
@@ -15,14 +15,40 @@ private:
 
 	Sprite2D m_img_background, m_img_stamina, m_img_staminaFrame;
 
+	struct TickSetting {
+		float slowdown = 0.00001f;
+		float gravity = 500.f;
+		float kickoff = 10.f;
+		float offset_width = 13;
+		float offset_height = 60;
+		float base_scale = 0.35f;
+	} m_tickSetting;
+
+	float m_hd_ticks_scalingVelocity[FruitType::NR_OF_FRUITS] = { 0 };
+	float m_hd_ticks_scaling[FruitType::NR_OF_FRUITS] = { 1 };
+	Sprite2D m_hd_back;
+	Sprite2D m_hd_ticks[FruitType::NR_OF_FRUITS];
+	Sprite2D m_hd_emptyTick;
+
+	struct Tick {
+		float scalingVelocity = 0;
+		float scale = 1;
+	};
+	vector<Tick> m_tickAnimations[FruitType::NR_OF_FRUITS];
+	int m_tick_previousCatchCount[FruitType::NR_OF_FRUITS] = { 0 };
+
 	Text2D m_text;
 
 	//-- Private Functions --
 
 	void drawTargetTime();
 
+	void imgui_tickSetting();
+
 public:
 	HUD();
+
+	void update(float dt);
 
 	void draw();
 };
